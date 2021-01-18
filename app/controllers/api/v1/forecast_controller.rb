4686b67495ee3584092181b456bcaf9422619d52
Zach Stearns <zach.stearns92@gmail.com>
2021-01-18 13:18:21 -0700

    Break up params to helper method, add Facade and Serializer methods to index

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    
+    forecast = ForecastFacade.get_weather_report(query_params[:location])
+    render json: ForecastSerializer.new(forecast)
+  end
+
+  def query_params
+    params.permit(:location)
   end
 end
