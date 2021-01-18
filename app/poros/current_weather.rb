543237e52624b9753efa0b44cc208dd01a75273e
Zach Stearns <zach.stearns92@gmail.com>
2021-01-17 23:41:32 -0700

    Remove kelvin to fahrenheit method

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -14,16 +14,12 @@ class CurrentWeather
     @datetime = Time.at(data[:dt])
     @sunrise = Time.at(data[:sunrise])
     @sunset = Time.at(data[:sunset])
-    @temperature = kelvin_to_fahrenheit(data[:temp])
-    @feels_like = kelvin_to_fahrenheit(data[:feels_like])
+    @temperature = data[:temp]
+    @feels_like = data[:feels_like]
     @humidity = data[:humidity]
     @uvi = data[:uvi]
     @visibility = data[:visibility]
     @conditions = data[:weather][0][:description]
     @icon = data[:weather][0][:icon]
-  end
-
-  def kelvin_to_fahrenheit(temp)
-    (temp - 273.15) * 9 / 5 + 32
   end
 end
