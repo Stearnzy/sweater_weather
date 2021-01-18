fad6f5dc5aea116e222d1e6b1e93c98187bbc9fa
Zach Stearns <zach.stearns92@gmail.com>
2021-01-18 12:31:49 -0700

    Put ForecastFacade.get_weather_report method test in vcr

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
 describe ForecastFacade do
-  it 'can retrieve current weather data from the Forecast Service using coordinates' do
-    VCR.use_cassette('ForecastService/returns_weather_from_coordinates') do
-      lat = 39.738453
-      long = -104.984853
+  it 'can retrieve current weather data from the Forecast Service using coordinates', :vcr do
+    location = 'denver,co'
 
-      forecast = ForecastFacade.get_weather_report(lat, long)
+    forecast = ForecastFacade.get_weather_report(location)
 
-      expect(forecast).to be_a Forecast
-      expect(forecast).to have_attributes(current_weather: CurrentWeather)
-      expect(forecast).to have_attributes(hourly_forecast: Array)
-      expect(forecast).to have_attributes(daily_forecast: Array)
-    end
+    expect(forecast).to be_a Forecast
+    expect(forecast).to have_attributes(current_weather: CurrentWeather)
+    expect(forecast).to have_attributes(hourly_forecast: Array)
+    expect(forecast).to have_attributes(daily_forecast: Array)
   end
 end
