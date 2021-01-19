38f3ce60efaf7e125d70af944abbd4671433a1ea
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 09:48:51 -0700

    Remove comments

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -1,12 +1,6 @@
 class MapquestFacade
   def self.location_to_coordinates(location)
-    # If location is valid,
-      data = MapquestService.location_coordinates(location)
-      coordinates = Coordinates.new(data)
-
-      
-    # else
-      # error message
-    # end
+    data = MapquestService.location_coordinates(location)
+    Coordinates.new(data)
   end
 end
