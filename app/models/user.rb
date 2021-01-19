b6071b76f1b422f143dce37cadc3a980ebec0eca
Zach Stearns <zach.stearns92@gmail.com>
2021-01-18 21:05:33 -0700

    Add back api_key uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
   validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true
-  validates :api_key, presence: true, uniqueness: true
+  validates :api_key, uniqueness: true
 end
