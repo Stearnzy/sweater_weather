adb2bcfbeecfe017cfc94647da657f70ae7d7e43
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 10:20:14 -0700

    initialize with destination attribute

---
+++ app/poros/destination_city.rb
@@ -0,0 +1,9 @@
+class DestinationCity
+  attr_reader :destination
+
+  def initialize(data)
+    @dest_city = data[:route][:locations][1][:adminArea5]
+    @dest_state = data[:route][:locations][1][:adminArea3]
+    @destination = "#{@dest_city}, #{@dest_state}"
+  end
+end
