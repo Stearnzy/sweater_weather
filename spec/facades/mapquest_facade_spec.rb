a23903e8a091530e371fdfc4acc120a9207752f4
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 15:32:44 -0700

    Add test for ETA

--- spec/facades/mapquest_facade_spec.rb
+++ spec/facades/mapquest_facade_spec.rb
@@ -11,4 +11,15 @@ describe MapquestFacade do
       expect(data).to have_attributes(long: Float)
     end
   end
+
+  it 'can get an ETA' do
+    VCR.use_cassette('MapquestService/returns_data_between_two_locations') do
+      origin = 'Denver, CO'
+      destination = 'Pueblo, CO'
+
+      unix_eta = MapquestFacade.arrival_time(origin, destination)
+      
+      expect(unix_eta).to be_an Integer
+    end
+  end
 end
