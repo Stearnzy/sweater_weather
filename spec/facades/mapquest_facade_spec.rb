db8f5cc7abb1467474d735361dfd2a23bf3388c4
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 15:46:13 -0700

    Add test for travel time

--- spec/facades/mapquest_facade_spec.rb
+++ spec/facades/mapquest_facade_spec.rb
@@ -18,8 +18,23 @@ describe MapquestFacade do
       destination = 'Pueblo, CO'
 
       unix_eta = MapquestFacade.arrival_time(origin, destination)
-      
+
       expect(unix_eta).to be_an Integer
+    end
+  end
+
+  it 'can get the travel time' do
+    VCR.use_cassette('MapquestService/returns_data_between_two_locations') do
+      origin = 'Denver, CO'
+      destination = 'Pueblo, CO'
+
+      travel_time = MapquestFacade.travel_time(origin, destination)
+
+      expect(travel_time).to be_a String
+      expect(travel_time).to_not be_empty
+      expect(travel_time.length).to eq(8)
+      expect(travel_time[2]).to eq(':')
+      expect(travel_time[5]).to eq(':')
     end
   end
 end
