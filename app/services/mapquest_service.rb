0b9c8521362e081ce2e16e2bcbf315d79c4db4af
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 15:20:29 -0700

    Add get_directions method

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -8,6 +8,16 @@ class MapquestService
     parse(response)
   end
 
+  def self.get_directions(trip_start, trip_end)
+    response = conn.get('/directions/v2/route') do |req|
+      req.params[:key] = ENV['MAPQUEST_KEY']
+      req.params[:from] = "#{trip_start}"
+      req.params[:to] = "#{trip_end}"
+    end
+
+    parse(response)
+  end
+
   def self.conn
     Faraday.new(url: 'http://www.mapquestapi.com')
   end
