be6e67972e96d6ea1a1396cb26006886c5e994ce
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 19:33:29 -0700

    Add conditionals to correct an impossible road trip

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,9 +1,13 @@
 class RoadTripFacade
   def self.new_road_trip(origin, destination)
     travel_time = MapquestFacade.travel_time(origin, destination)
-    unix_eta = MapquestFacade.arrival_time(origin, destination).to_i
-    weather_at_destination = ForecastFacade.trip_future_weather(unix_eta, destination)
+    if travel_time != 'Impossible'
+      unix_eta = MapquestFacade.arrival_time(origin, destination)
+      weather_at_destination = ForecastFacade.trip_future_weather(unix_eta, destination)
+    else
+      weather_at_destination = nil
+    end
 
-    RoadTrip.new(travel_time, unix_eta, weather_at_destination, origin, destination)
+    RoadTrip.new(travel_time, weather_at_destination, origin, destination)
   end
 end
