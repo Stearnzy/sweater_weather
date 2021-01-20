205b6eb150dd76b92a285dc74469d80cb5890cf0
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 17:57:05 -0700

    Test new_road_trip

---
+++ spec/facades/road_trip_facade_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'RoadTripFacade', :vcr do
+  it 'gets road trip information' do
+    origin = 'Denver, CO'
+    destination = 'Pueblo, CO'
+
+    trip = RoadTripFacade.new_road_trip(origin, destination)
+
+    expect(trip).to have_attributes(start_city: String)
+    expect(trip).to have_attributes(end_city: String)
+    expect(trip).to have_attributes(travel_time: String)
+    expect(trip).to have_attributes(weather_at_eta: Hash)
+  end
+end
