4300cfacecd3e37a73cb997490f632e552785bbf
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 19:18:57 -0700

    make travel_time return impossible if theres a status code besides 0

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -13,11 +13,15 @@ class MapquestFacade
   def self.arrival_time(origin, destination)
     data = MapquestService.get_directions(origin, destination)
     travel_time = data[:route][:realTime]
-    Time.now.to_i + travel_time
+    (Time.now.to_i + travel_time).to_i
   end
 
   def self.travel_time(origin, destination)
     data = MapquestService.get_directions(origin, destination)
-    data[:route][:formattedTime]
+    if data[:info][:statuscode] == 0
+      data[:route][:formattedTime]
+    else
+      'Impossible'
+    end
   end
 end
