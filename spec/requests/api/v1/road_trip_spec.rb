c1e22badc45b7a85305022d2f24e89b5c7875c5b
Zach Stearns <zach.stearns92@gmail.com>
2021-01-19 19:37:08 -0700

    Sad test for impossible road trip

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -76,4 +76,47 @@ describe 'Road Trip Create', :vcr do
     expect(parsed).to have_key(:error)
     expect(parsed[:error]).to eq('Invalid Api Key')
   end
+
+  it 'shows Impossible for an impossible road trip' do
+    body = {
+      "origin": 'New York, NY',
+      "destination": 'London, UK',
+      "api_key": @user.api_key.to_s
+    }
+
+    post '/api/v1/road_trip', headers: @headers, params: JSON.generate(body)
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:data)
+    expect(parsed[:data]).to be_a Hash
+
+    expect(parsed[:data]).to have_key(:id)
+    expect(parsed[:data][:id]).to eq(nil)
+
+    expect(parsed[:data]).to have_key(:type)
+    expect(parsed[:data][:type]).to be_a String
+    expect(parsed[:data][:type]).to eq('road_trip')
+
+    expect(parsed[:data]).to have_key(:attributes)
+    expect(parsed[:data][:attributes]).to be_a Hash
+
+    expect(parsed[:data][:attributes]).to have_key(:start_city)
+    expect(parsed[:data][:attributes][:start_city]).to be_a String
+    expect(parsed[:data][:attributes][:start_city]).to eq('New York, NY')
+
+    expect(parsed[:data][:attributes]).to have_key(:end_city)
+    expect(parsed[:data][:attributes][:end_city]).to be_a String
+    expect(parsed[:data][:attributes][:end_city]).to eq('London, UK')
+
+    expect(parsed[:data][:attributes]).to have_key(:travel_time)
+    expect(parsed[:data][:attributes][:travel_time]).to be_a String
+    expect(parsed[:data][:attributes][:travel_time]).to eq('Impossible')
+
+    expect(parsed[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(parsed[:data][:attributes][:weather_at_eta]).to be_a Hash
+    expect(parsed[:data][:attributes][:weather_at_eta]).to be_empty
+  end
 end
